86a207ea99da4158356bacc27a82f0e8de95e4fb
Pradeep Balan Pillai <pradeepbp@outlook.com>
2018-09-18 13:58:21 +0530

    Comman line input enabled

--- src/IncBackup.java
+++ src/IncBackup.java
@@ -26,13 +26,22 @@ import bkp.*;
 
  class IncBackup{
      public static void main(String[] args){
-         System.out.println("App Initialised...");
-
-         //Try creating a new backup location
-         ConfigOp newOp = new ConfigOp();
-         System.out.println("Class created");
-        // System.out.println(newOp.createPath("~/temp1/bkp").toString());
-         System.out.println(newOp.createBkpLocation(newOp.createPath("/home/pradeep/temp1/bkp")));
+        System.out.println("App Initialised...");
+		String input = null;
+		if(args.length > 0){
+			input = args[0];
+		}
+		System.out.println("Path is " + input);
+        
+		//Try creating a new backup location
+        if(input != null){
+			ConfigOp newOp = new ConfigOp();
+			System.out.println("Class created");
+			// System.out.println(newOp.createPath("~/temp1/bkp").toString());
+			System.out.println(newOp.createBkpLocation(newOp.createPath(input)));
+		}
+		else
+			System.out.println("No path specified. Try again with a valid backup location");
      }// main function
 
  } // main class ends

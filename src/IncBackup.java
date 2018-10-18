3e7ef344151019e69a304fa85672993a19e69fc0
Pradeep Balan Pillai <pradeepbp@outlook.com>
2018-10-19 00:42:36 +0530

    Partially added command line interaction capability

--- src/IncBackup.java
+++ src/IncBackup.java
@@ -27,80 +27,16 @@ import java.nio.file.*;
 import java.util.*;
 import java.io.*;
 
- class IncBackup{
-
+class IncBackup{
      
 
-     public static void main(String[] args){
+    public static void main(String[] args){
         
-        Path backupLocPath;
-       /* 
-        System.out.println("App Initialised...");
-        //ConfigOp newOp = new ConfigOp();
-		String input = null;
-		if(args.length > 0){
-			input = args[0];
-		}
-		System.out.println("Path is " + input);
-        
-		//Try creating a new backup location
-        ConfigOp newOp = null;
-        if(input != null){
-			newOp = new ConfigOp(input);
-		}
-		else
-			System.out.println("No path specified. Try again with a valid backup location");
+        new IncBkpCmd().AppIntroCmd();
 
+    }// main function
 
-        System.out.println(newOp.getBackupLocation());
-        newOp.createCsvDataBase("csvdata");
-        */
-
-        AppIntroCmd();
-     }// main function
-
-    
-    public static int AppIntroCmd(){
-        Path listPath = Paths.get("bkplist");
-        List<String> list =  null;
-        
-        System.out.println("Availabe backups are:");
-        int i = 1;
-        try{
-            list  = Files.readAllLines(listPath);
-            
-            for(String item: list){
-                System.out.format("(%d) %s\n", i, item);
-                i = i + 1;
-            }       
-            
-        }
-        catch(IOException ioe){
-            System.err.println("ERROR: Cannot open backup list");
-        }
-                    
-        
-        int selection = 0;
-        
-        while(true){
-            try{
-                System.out.print("Select backup location:");
-                Scanner scan = new Scanner(System.in);
-                selection = scan.nextInt();
-                break;
-            }
-            catch(InputMismatchException ime){
-                System.out.println("Bad input; try again.");
-            }
-            
-            
-        }
-        
-        System.out.println("Selected " + list.get(selection - 1));
-        return 0;
-    } // end of function
-
- } // main class ends
+} // main class ends
 
  
 
---
+++ src/IncBkpCmd.java
@@ -0,0 +1,75 @@
+
+import java.io.*;
+import java.util.*;
+import java.nio.file.*;
+
+/**
+        Command line operations of backup utility
+
+ */
+
+
+public class IncBkpCmd{
+
+    /**
+        Present an interactive session for user to select the backup location of her choice
+     */
+
+    public int AppIntroCmd(){
+            Path listPath = Paths.get("bkplist");
+            List<String> list =  null;
+            
+            System.out.println("Availabe backups are:");
+            int i = 1;
+            try{
+                list  = Files.readAllLines(listPath);
+                list.add("-> Exit");
+                
+                for(String item: list){
+                    System.out.format("(%d) %s\n", i, item);
+                    i = i + 1;
+                }       
+                
+            }
+            catch(IOException ioe){
+                System.err.println("ERROR: Cannot open backup list");
+            }
+                        
+            int selection = 0;
+            
+            while(true){
+                try{
+                    System.out.print("Select Option:");
+                    Scanner scan = new Scanner(System.in);
+                    selection = new Integer(scan.next());
+                    checkRange(selection, list.size());
+                    break;
+                }
+                catch(InputMismatchException ime){
+                    System.out.println("Bad input; try again.");
+                }          
+                
+            }
+            
+            System.out.println("Selected " + list.get(selection - 1));
+            return selection;
+        } // end of function
+
+        /**
+            This function checks whether the selected option is within the available optons or not
+         */
+        
+        public void checkRange(int selection, int limit){
+            if(selection > limit){
+                throw new InputMismatchException();
+            }
+        }
+
+        
+        /**
+            Interact with user; user selects the operation
+        
+         */
+       
+
+} // end of class
